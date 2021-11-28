210a5ac49f812b04f1ef7f79639b0e834ecc324e
Victor Volkov <vityan2504@gmail.com>
2021-11-28 16:54:33 +0300

    произвёл операции со значениями переменных и вывед в консоль

--- script/script.js
+++ script/script.js
@@ -1,8 +1,26 @@
 'use strict';
 
+// 1-е задание
 let money = 30000,
    income = 'Фриланс',
-   addExpenses = 'проездной, квартира, еда',
+   addExpenses = 'Проездной, Квартира, Eда',
    deposit = false,
    mission = 300000,
-   period = 12;
+   period = 12,
+   budgetDay = money / 30;
+
+// 2-е задание
+console.log( typeof money );
+console.log( typeof income );
+console.log( typeof deposit );
+
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев.
+Цель заработать ${mission} рублей.`);
+
+console.log(addExpenses.toLocaleLowerCase().split(', '));
+
+console.log(budgetDay);
+
+// 3-е задание
